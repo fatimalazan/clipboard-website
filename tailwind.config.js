dd7ce8ed6902f41b1162c18f498bce26466fc111
fatimalazan <fatima615.lazan@gmail.com>
2023-03-02 14:55:20 -0600

    Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ['./*html'],
   theme: {
-    extend: {},
+    screens: {
+      sm: '375px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+    extend: {
+      colors: {
+        strongCyan: 'hsl(171, 66%, 44%)',
+        lightBlue: 'hsl(233, 100%, 69%)',
+        darkGrayishBlue: 'hsl(210, 10%, 33%)',
+        grayishBlue: 'hsl(201, 11%, 66%)',
+      },
+      fontFamily: {
+        sans: ['Bai Jamjuree', 'sans-serif'],
+      },
+      backgroundImage: {
+        'hero-img': 'url(/images/bg-header-mobile.png)',
+      },
+      boxShadow: {
+        'green-3xl': '0px 10px 20px rgba(137, 229, 199, 0.503305), inset 0px -3px 0px rgba(0, 0, 0, 0.151333)',
+        'blue-3xl': '0px 10px 20px rgba(0, 0, 0, 0.0988035), inset 0px -3px 0px rgba(0, 0, 0, 0.151333)',
+      },
+    },
   },
   plugins: [],
 };
